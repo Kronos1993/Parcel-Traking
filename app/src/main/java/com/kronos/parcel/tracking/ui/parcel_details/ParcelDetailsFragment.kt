c56b0a8c12eca282fc8fcdd070ddffff30159e86
mguerral <mguerral>
2024-02-09 12:58:16 -0500

    Using binding adapters to handle status image

--- app/src/main/java/com/kronos/parcel/tracking/ui/parcel_details/ParcelDetailsFragment.kt
+++ app/src/main/java/com/kronos/parcel/tracking/ui/parcel_details/ParcelDetailsFragment.kt
@@ -107,7 +107,6 @@ class ParcelDetailsFragment : Fragment() {
     }
 
     private fun handleParcel(parcel: ParcelModel) {
-        Glide.with(requireContext()).load(viewModel.getImageUrl(parcel)).placeholder(R.drawable.ic_not_found).into(binding.imageStatus)
     }
 
     private fun handleEventList(list: List<EventModel>) {
