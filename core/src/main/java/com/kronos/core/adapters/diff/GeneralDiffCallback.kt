5f4cdf70005ff90740c10a1ada8aff5daebc93fb
mguerral <mguerral>
2024-03-21 16:54:50 -0500

    Changes in class T

--- core/src/main/java/com/kronos/core/adapters/diff/GeneralDiffCallback.kt
+++ core/src/main/java/com/kronos/core/adapters/diff/GeneralDiffCallback.kt
@@ -1,13 +1,14 @@
 package com.kronos.core.adapters.diff
 
+import android.annotation.SuppressLint
 import androidx.recyclerview.widget.DiffUtil
-
-class GeneralDiffCallback<T>:DiffUtil.ItemCallback<T>() {
+class GeneralDiffCallback<T : Any>: DiffUtil.ItemCallback<T>() {
     override fun areItemsTheSame(oldItem: T, newItem: T): Boolean {
         return oldItem == newItem
     }
 
+    @SuppressLint("DiffUtilEquals")
     override fun areContentsTheSame(oldItem: T, newItem: T): Boolean {
-        return oldItem.hashCode() == newItem.hashCode()
+        return newItem.hashCode() == oldItem.hashCode()
     }
 }
