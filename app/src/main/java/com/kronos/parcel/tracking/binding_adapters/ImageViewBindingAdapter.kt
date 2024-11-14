f57a7bbb8a6447cfb554debb5e9d531970b96cae
kronos <mguerral1993@gmail.com>
2024-11-13 21:43:21 -0500

    new svgs

--- app/src/main/java/com/kronos/parcel/tracking/binding_adapters/ImageViewBindingAdapter.kt
+++ app/src/main/java/com/kronos/parcel/tracking/binding_adapters/ImageViewBindingAdapter.kt
@@ -8,14 +8,14 @@ import com.kronos.parcel.tracking.R
 fun handleParcelStatus(view: ImageView, parcelStatus: String?) = view.run {
     if (parcelStatus != null) {
         when (parcelStatus) {
-            "Carga recibida en Miami" -> view.setBackgroundResource(R.mipmap.carga_recibida)
-            "Carga entregada a agente Aereo" -> view.setBackgroundResource(R.mipmap.entregada_agente_aereo)
-            "Carga en Proceso de Aduanas" -> view.setBackgroundResource(R.mipmap.proceso_aduanas)
-            "Carga en Bodega Central" -> view.setBackgroundResource(R.mipmap.carga_transito_camion)
-            "Carga disponible para entrega en sucursal" -> view.setBackgroundResource(R.mipmap.disponible_entrega)
+            "Carga recibida en Miami" -> view.setBackgroundResource(R.drawable.carga_recibida)
+            "Carga entregada a agente Aereo" -> view.setBackgroundResource(R.drawable.entregada_agente_aereo)
+            "Carga en Proceso de Aduanas" -> view.setBackgroundResource(R.drawable.proceso_aduanas)
+            "Carga en Bodega Central" -> view.setBackgroundResource(R.drawable.carga_transito_camion)
+            "Carga disponible para entrega en sucursal" -> view.setBackgroundResource(R.drawable.disponible_entrega)
             "not found" -> view.setBackgroundResource(R.drawable.ic_not_found)
             "" -> view.setBackgroundResource(R.drawable.ic_not_found)
-            else -> view.setBackgroundResource(R.mipmap.entregado)
+            else -> view.setBackgroundResource(R.drawable.entregado)
         }
     }else
         view.setBackgroundResource(R.drawable.ic_not_found)
